a5aef156677ead9e8f775828c4187470805bf566
kliu <kingson708@gmail.com>
2018-04-07 12:37:28 +0800

    add tests

--- page-sort/src/main/java/com/github/lkq/PageSort.java
+++ page-sort/src/main/java/com/github/lkq/PageSort.java
@@ -1,4 +1,10 @@
 package com.github.lkq;
 
+import java.util.Collections;
+import java.util.List;
+
 public class PageSort {
+    public List<Integer> sort(List<Integer> list, int startIndex, int endIndex) {
+        return Collections.emptyList();
+    }
 }
--- page-sort/src/test/java/com/github/lkq/PageSortTest.java
+++ page-sort/src/test/java/com/github/lkq/PageSortTest.java
@@ -1,5 +1,59 @@
 package com.github.lkq;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 class PageSortTest {
 
+    private PageSort pageSort;
+    private List<Integer> unsortedList;
+
+    @BeforeEach
+    void setUp() {
+        pageSort = new PageSort();
+        unsortedList = createUnsortedList();
+    }
+
+    @Test
+    void canSortFirstPage() {
+        runTest(0, 99);
+    }
+
+    @Test
+    void canSortPageInTheMiddle() {
+        runTest(500, 599);
+    }
+
+    @Test
+    void canSortLastPage() {
+        runTest(900, 999);
+    }
+
+    @Test
+    void canSortLastHalfPage() {
+        runTest(900, 1099);
+    }
+
+    private void runTest(int startIndex, int endIndex) {
+        ArrayList<Integer> sortedList = new ArrayList<>(unsortedList);
+        sortedList.sort(Comparator.naturalOrder());
+        List<Integer> sortedData = pageSort.sort(unsortedList, startIndex, endIndex);
+
+        Integer[] expected = sortedList.subList(startIndex, endIndex).toArray(new Integer[0]);
+        Integer[] actual = sortedData.toArray(new Integer[0]);
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    private List<Integer> createUnsortedList() {
+        List<Integer> unsortedList = new ArrayList<>(1000);
+        for (Integer index : unsortedList) {
+            unsortedList.add((int) (Math.random() * 100000));
+        }
+        return unsortedList;
+    }
 }
